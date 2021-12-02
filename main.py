26c3fd4d0d949b6950d78018631958adf843f03e
RJ <rjbatuigas@gmail.com>
2021-12-01 20:28:52 -0500

    changed postgresql database uri

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class BlogPost(db.Model):
     comments = relationship("Comment", back_populates="parent_post")
 
 
-#db.create_all()
+db.create_all()
 
 class User(UserMixin, db.Model):
     __tablename__ = "user"
